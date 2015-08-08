ae9f1bbd488405373148a933ed6ff933c78f7c00
qylibohao <375795423@qq.com>
2015-08-08 17:15:58 +0800

    config vc

--- BHNavigation/BHNavigation/BHViewController.m
+++ BHNavigation/BHNavigation/BHViewController.m
@@ -14,39 +14,56 @@ static NSInteger pushedCount = 0;
 
 @interface BHViewController ()
 
+@property (nonatomic, strong) UILabel  *switchLabel;
+@property (nonatomic, strong) UIButton *pushButton;
+@property (nonatomic, strong) UISwitch *switchCtrl;
+
 @end
 
 @implementation BHViewController
 
+#pragma mark - Property
+
+- (UILabel* )switchLabel {
+    if (!_switchLabel) {
+        _switchLabel = [[UILabel alloc] initWithFrame:(CGRect){20, 153, 100, 44}];
+        _switchLabel.font = [UIFont systemFontOfSize:15];
+        _switchLabel.text = @"隐藏导航栏 :";
+    }
+    return _switchLabel;
+}
+
+- (UISwitch* )switchCtrl {
+    if (!_switchCtrl) {
+        _switchCtrl = [[UISwitch alloc] initWithFrame:(CGRect){160, 160, 90, 44}];
+        [_switchCtrl addTarget:self action:@selector(switchChange:) forControlEvents:UIControlEventValueChanged];
+        _switchCtrl.on = NO;
+    }
+    return _switchCtrl;
+}
+
+- (UIButton* )pushButton {
+    if (!_pushButton) {
+        _pushButton = [[UIButton alloc] initWithFrame:(CGRect){20, 230, 200, 44}];
+        [_pushButton setTitle:@"push" forState:UIControlStateNormal];
+        [_pushButton setTitleColor:[self randomColor] forState:UIControlStateNormal];
+        _pushButton.backgroundColor = [self randomColor];
+        [_pushButton addTarget:self action:@selector(pushToNextVC) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _pushButton;
+}
+
+#pragma mark - Life Cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    CGFloat red = arc4random() / (CGFloat)INT_MAX;
-    CGFloat green = arc4random() / (CGFloat)INT_MAX;
-    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
-    self.view.backgroundColor = [UIColor colorWithRed:red
-                                               green:green
-                                                 blue:blue
-                                                alpha:1.0];
+    self.view.backgroundColor = [self randomColor];
     
-    
-    
-    UILabel *switchLabel = [[UILabel alloc] initWithFrame:(CGRect){20, 153, 100, 44}];
-    switchLabel.font = [UIFont systemFontOfSize:15];
-    switchLabel.text = @"隐藏导航栏 :";
-    [self.view addSubview:switchLabel];
-    
-    UISwitch *aSwitch = [[UISwitch alloc] initWithFrame:(CGRect){160, 160, 90, 44}];
-    [aSwitch addTarget:self action:@selector(switchChange:) forControlEvents:UIControlEventValueChanged];
-    [self.view addSubview:aSwitch];
-    aSwitch.on = NO;
-    
-    UIButton *pushButton = [[UIButton alloc] initWithFrame:(CGRect){20, 230, 200, 44}];
-    [pushButton setTitle:@"push" forState:UIControlStateNormal];
-    [pushButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    pushButton.backgroundColor = [UIColor grayColor];
-    [pushButton addTarget:self action:@selector(pushToNextVC) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:pushButton];
+    [self.view addSubview:self.switchLabel];
+    [self.view addSubview:self.switchCtrl];
+    self.switchCtrl.on = NO;
+    [self.view addSubview:self.pushButton];
     
     self.bh_NavigationItem.title = [NSString stringWithFormat:@"第 %ld 页", (long)pushedCount];
     
@@ -67,8 +84,20 @@ static NSInteger pushedCount = 0;
     [super didReceiveMemoryWarning];
 }
 
+
+#pragma mark - Functoions
+
+- (UIColor*)randomColor {
+    CGFloat red = arc4random() / (CGFloat)INT_MAX;
+    CGFloat green = arc4random() / (CGFloat)INT_MAX;
+    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
+    return [UIColor colorWithRed:red
+                            green:green
+                             blue:blue
+                            alpha:1.0];
+}
+
 - (void)pushToNextVC {
-    
     BHViewController *vc = [[BHViewController alloc] init];
     pushedCount ++;
     [self.navigationController pushViewController:vc animated:YES];
